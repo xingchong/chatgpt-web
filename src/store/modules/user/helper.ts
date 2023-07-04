f2c016ed6b199bcc0d112ad75293aec432b9ffbb
DESKTOP-TONGZHOU\admin <xingchong6@163.com>
2023-07-04 22:23:14 +0800

    fix: profile modify

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://profile-avatar.csdnimg.cn/a120f0df426b49038d5d3373f226040a_meilinghu.jpg',
+      name: 'chong',
+      description: 'Star on <a href="https://github.com/xingchong/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
