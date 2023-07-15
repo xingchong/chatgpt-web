40ec19ba37cf853f2714ce4ff18bbb30066f4c55
DESKTOP-TONGZHOU\admin <xingchong6@163.com>
2023-07-15 08:03:51 +0800

    fix: profile modify

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -480,8 +480,8 @@ onUnmounted(() => {
         >
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
-              <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <!-- <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" /> -->
+              <span>hi, boy~</span>
             </div>
           </template>
           <template v-else>
